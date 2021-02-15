58fd59d26a2361a66a9c8e475ebeccf396c90190
PeratX <peratx@itxtech.org>
2021-02-15 15:33:18 +0800

    App: happy new year

--- app/src/main/java/org/itxtech/flashmaster/MainActivity.kt
+++ app/src/main/java/org/itxtech/flashmaster/MainActivity.kt
@@ -2,19 +2,20 @@
  *
  * FlashMasterAndroid
  *
- * Copyright (C) 2019-2020 iTX Technologies
+ * Copyright (C) 2019-2021 iTX Technologies
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  * @author PeratX
  *
@@ -29,10 +30,7 @@ import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Build
-import android.os.Bundle
-import android.os.Environment
-import android.os.Handler
+import android.os.*
 import android.provider.MediaStore
 import android.view.View
 import android.webkit.WebView
@@ -41,65 +39,68 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity_main.*
+import org.itxtech.flashmaster.databinding.ActivityMainBinding
 import java.io.File
 import java.io.FileOutputStream
-import java.io.OutputStream
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     private lateinit var file: String
+    private lateinit var binding: ActivityMainBinding
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-        webview.settings.apply {
-            setAppCacheEnabled(true)
-            setAppCachePath(application.cacheDir.absolutePath)
-            javaScriptEnabled = true
-            domStorageEnabled = true
-            allowFileAccess = true
-            databaseEnabled = true
-            loadsImagesAutomatically = true
-            allowUniversalAccessFromFileURLs = true
-        }
-        webview.webViewClient = object : WebViewClient() {
-            override fun doUpdateVisitedHistory(view: WebView?, url: String?, isReload: Boolean) {
-                if (url!!.startsWith("file:///android_asset/index.html#/about")) {
-                    AlertDialog.Builder(this@MainActivity)
-                        .setTitle(R.string.about_title)
-                        .setMessage(
-                            getString(R.string.about)
-                                .replace("ver", BuildConfig.VERSION_NAME + " (" + BuildConfig.VERSION_CODE + ")")
-                                .replace("rev", BuildConfig.GIT_COMMIT)
-                        )
-                        .setNegativeButton("GitHub") { _, _ -> openUri("https://github.com/iTXTech/FlashMasterAndroid") }
-                        .setPositiveButton(android.R.string.cancel, null)
-                        .show()
+        with(binding.webview) {
+            settings.apply {
+                setAppCacheEnabled(true)
+                setAppCachePath(application.cacheDir.absolutePath)
+                javaScriptEnabled = true
+                domStorageEnabled = true
+                allowFileAccess = true
+                databaseEnabled = true
+                loadsImagesAutomatically = true
+                allowUniversalAccessFromFileURLs = true
+            }
+            webViewClient = object : WebViewClient() {
+                override fun doUpdateVisitedHistory(view: WebView?, url: String?, isReload: Boolean) {
+                    if (url!!.startsWith("file:///android_asset/index.html#/about")) {
+                        AlertDialog.Builder(this@MainActivity)
+                            .setTitle(R.string.about_title)
+                            .setMessage(
+                                getString(R.string.about)
+                                    .replace("ver", BuildConfig.VERSION_NAME + " (" + BuildConfig.VERSION_CODE + ")")
+                                    .replace("rev", BuildConfig.GIT_COMMIT)
+                            )
+                            .setNegativeButton("GitHub") { _, _ -> openUri("https://github.com/iTXTech/FlashMasterAndroid") }
+                            .setPositiveButton(android.R.string.cancel, null)
+                            .show()
+                    }
+                    super.doUpdateVisitedHistory(view, url, isReload)
                 }
-                super.doUpdateVisitedHistory(view, url, isReload)
-            }
 
-            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                if (!url!!.startsWith("file://")) {
-                    openUri(url)
-                    return true
+                override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                    if (!url!!.startsWith("file://")) {
+                        openUri(url)
+                        return true
+                    }
+                    return false
                 }
-                return false
-            }
 
-            override fun onPageFinished(view: WebView?, url: String?) {
-                Handler().postDelayed({
-                    view?.visibility = View.VISIBLE
-                }, 200)
-                super.onPageFinished(view, url)
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        view?.visibility = View.VISIBLE
+                    }, 200)
+                    super.onPageFinished(view, url)
+                }
             }
+            setDownloadListener { url, _, _, _, _ ->
+                save(url)
+            }
+            loadUrl("file:///android_asset/index.html")
         }
-        webview.setDownloadListener { url, _, _, _, _ ->
-            save(url)
-        }
-        webview.loadUrl("file:///android_asset/index.html")
     }
 
     private fun save(url: String) {
@@ -120,8 +121,7 @@ class MainActivity : AppCompatActivity() {
     private fun write() {
         val stream = assets.open(file)
         val fileName = file.substringAfter("/")
-        val os: OutputStream?
-        os = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+        val os = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
             FileOutputStream(
                 File(
                     Environment.getExternalStoragePublicDirectory(
@@ -144,7 +144,7 @@ class MainActivity : AppCompatActivity() {
             }
             os?.close()
             runOnUiThread {
-                Snackbar.make(view, R.string.fileStored, Snackbar.LENGTH_LONG).show()
+                Snackbar.make(binding.view, R.string.fileStored, Snackbar.LENGTH_LONG).show()
             }
         }
     }
@@ -154,7 +154,7 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == 1) {
             for (result in grantResults) {
                 if (result != PackageManager.PERMISSION_GRANTED) {
-                    Snackbar.make(view, R.string.permDenied, Snackbar.LENGTH_LONG).show()
+                    Snackbar.make(binding.view, R.string.permDenied, Snackbar.LENGTH_LONG).show()
                     return
                 }
             }
@@ -166,8 +166,8 @@ class MainActivity : AppCompatActivity() {
         startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(uri)))
 
     override fun onBackPressed() {
-        if (webview.canGoBack()) {
-            webview.goBack()
+        if (binding.webview.canGoBack()) {
+            binding.webview.goBack()
         } else {
             super.onBackPressed()
         }
